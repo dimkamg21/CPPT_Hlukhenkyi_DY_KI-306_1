b849075581f8105de2d8a3064e5031d8b210cb29
Dmytro <dmytro2105@gmail.com>
2023-09-20 23:34:44 +0300

    upload lab5

---
+++ lab4/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"KI306.Hlukhenkyi.lab4"}];updateSearchResults();
---
+++ lab4/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI306.Hlukhenkyi.lab4","l":"ExpressionCalculator"},{"p":"KI306.Hlukhenkyi.lab4","l":"Main"}];updateSearchResults();
